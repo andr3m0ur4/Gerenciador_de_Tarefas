6f896846cff23379e5f04b1d1911ed1d23d8c716
André Moura <andre.benedicto@etec.sp.gov.br>
2021-03-09 09:33:18 -0300

    finalizando README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Gerenciador_de_Tarefas
-Aplicação web responsável por armazenar tarefas do usuário!
+
+Web application responsible by store users tasks inside it!
+
+## Installation
+
+### Load DataBase
+
+First, import the database to your MySQL Server, the file sql is in this directory:
+```
+./base_dados/tarefas.sql
+```
+
+### Server PHP
+
+After, use your terminal to execute the application.
+```bash
+php -S localhost:8080
+```
+
+## Usage
+
+Then, open [Gerenciador de Tarefas](http://localhost:8080) in your browser.
--- base_dados/tarefas.sql
+++ base_dados/tarefas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 10-Out-2019 às 20:05
--- Versão do servidor: 10.3.16-MariaDB
--- versão do PHP: 7.3.7
+-- Generation Time: 09-Mar-2021 às 12:31
+-- Versão do servidor: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,11 +19,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `tarefas`
+-- Database: `tarefas`
 --
-
-CREATE DATABASE IF NOT EXISTS tarefas;
-USE tarefas;
+CREATE DATABASE IF NOT EXISTS `tarefas` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `tarefas`;
 
 -- --------------------------------------------------------
 
@@ -55,7 +54,7 @@ INSERT INTO `anexos` (`id`, `tarefa_id`, `nome`, `arquivo`) VALUES
 CREATE TABLE `tarefas` (
   `id` int(11) NOT NULL,
   `nome` varchar(20) NOT NULL,
-  `descricao` text DEFAULT NULL,
+  `descricao` text,
   `prazo` date DEFAULT NULL,
   `prioridade` int(1) DEFAULT NULL,
   `concluida` tinyint(1) DEFAULT NULL
@@ -66,49 +65,49 @@ CREATE TABLE `tarefas` (
 --
 
 INSERT INTO `tarefas` (`id`, `nome`, `descricao`, `prazo`, `prioridade`, `concluida`) VALUES
-(1, 'Estudar PHP', 'Continuar meus estudos	de PHP e MySQL', '2019-10-20', 3, 0),
 (3, 'Comprar leite', 'Desnatado', '0000-00-00', 1, 0),
-(4, 'Arrumar as fotos da ', 'SÃ³ quando der tempo', '0000-00-00', 3, 0),
-(5, 'Estudar MySQL', 'Cadasrado pelo formulÃ¡rio', NULL, 1, NULL),
-(8, 'Prova de PHP 2', 'Estudar atÃ© o prazo', '0000-00-00', 1, 0),
-(9, 'Prova de PHP', 'Estudar atÃ© o prazo', '2019-10-15', 2, NULL),
-(11, 'Estudar HTML', 'HTML Ã© muito importante', '2019-10-16', 2, 0),
+(4, 'Arrumar as fotos da ', 'Só quando der tempo', '0000-00-00', 3, 0),
+(5, 'Estudar MySQL', 'Cadastrado pelo formulário', '0000-00-00', 1, 0),
+(8, 'Prova de PHP 2', 'Estudar até o prazo', '0000-00-00', 1, 0),
+(9, 'Prova de PHP', 'Estudar até o prazo', '2019-10-15', 2, 0),
+(11, 'Estudar HTML', 'HTML é muito importante', '2019-10-16', 2, 0),
 (12, 'Teste', 'Realizando um teste', '2019-12-08', 1, 0),
-(14, 'Testar arquivo base', 'CriaÃ§Ã£o do arquivo base.php contendo dados do formulÃ¡rio', '2019-10-07', 3, 1),
-(15, 'Estudar CSS', 'DescriÃ§Ã£o da minha tarefa', '2019-10-22', 2, 0),
-(17, 'Estudar CSS', 'DescriÃ§Ã£o da minha tarefa', '2019-10-22', 2, 0);
+(14, 'Testar arquivo base', 'Criação do arquivo base.php contendo dados do formulário', '2019-10-07', 3, 1),
+(15, 'Estudar CSS', 'Descrição da minha tarefa', '2019-10-22', 2, 0),
+(17, 'Estudar CSS', 'Descrição da minha tarefa', '2019-10-22', 2, 0),
+(18, 'README', 'Criar arquivo README para este projeto', '2021-03-09', 3, 0);
 
 --
--- Índices para tabelas despejadas
+-- Indexes for dumped tables
 --
 
 --
--- Índices para tabela `anexos`
+-- Indexes for table `anexos`
 --
 ALTER TABLE `anexos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices para tabela `tarefas`
+-- Indexes for table `tarefas`
 --
 ALTER TABLE `tarefas`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de tabela `anexos`
+-- AUTO_INCREMENT for table `anexos`
 --
 ALTER TABLE `anexos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de tabela `tarefas`
+-- AUTO_INCREMENT for table `tarefas`
 --
 ALTER TABLE `tarefas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
